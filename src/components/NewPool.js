e475a531592355bf07f7640f45629959860c7864
Nicholas <nicholas.allio@me.com>
2018-08-03 22:59:40 +0200

    Created NewPool component with initial implementation

---
+++ src/components/NewPool.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewPool extends Component {
+  render() {
+    return(
+      <div>
+        NewPool
+      </div>
+    )
+  }
+}
+
+export default NewPool
