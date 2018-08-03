e6f00cdf729219d0cee73f61b8d36bc3048d854b
Nicholas <nicholas.allio@me.com>
2018-08-03 23:23:17 +0200

    Imported Pool component and added dummy logic to display content

--- src/components/PoolList.js
+++ src/components/PoolList.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import Pool from './Pool'
 
 class PoolList extends Component {
   render() {
+    const { pools } = this.props
+
     return(
       <div>
         PoolList
+        <ul>
+          {pools.map((p) => (
+            <li key={p.id}>
+              <Pool name={p.name} />
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
