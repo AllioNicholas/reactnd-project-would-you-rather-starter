b776bfa03e28e9d551ca72271cdf5d5f317d3f97
Nicholas <nicholas.allio@me.com>
2018-08-03 22:58:14 +0200

    Created PoolPage component file and first fuzy implementation

---
+++ src/components/PoolPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PoolPage extends Component {
+  render() {
+    return (
+      <div>
+        PoolPage
+      </div>
+    )
+  }
+}
+
+export default PoolPage
