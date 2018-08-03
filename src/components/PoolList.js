b34537e00f8443cfb7d9145a5e9facd89449821a
Nicholas <nicholas.allio@me.com>
2018-08-03 23:00:15 +0200

    Created PoolList component with first dummy implementation

---
+++ src/components/PoolList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PoolList extends Component {
+  render() {
+    return(
+      <div>
+        PoolList
+      </div>
+    )
+  }
+}
+
+export default PoolList
