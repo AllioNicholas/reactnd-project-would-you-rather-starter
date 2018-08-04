1076d2a78ba9ed810a7d7237c525447f23ef5774
Nicholas <nicholas.allio@me.com>
2018-08-04 20:04:23 +0200

    Added getPools action creator

--- src/actions/pools.js
+++ src/actions/pools.js
@@ -1,5 +1,29 @@
 import * as API from './../utils/_DATA'
+// import { showLoading, hideLoading } from 'react-redux-loading'
 
 export const GET_POOLS = 'GET_POOLS'
 export const ADD_POOL = 'ADD_POOL'
 export const ADD_ANSWER = 'ADD_ANSWER'
+
+export function getPools(pools) {
+  return {
+    type: GET_POOLS,
+    tweets
+  }
+}
+
+// function addPool(pool) {
+//   return {
+//     type: ADD_POOL,
+//     pool
+//   }
+// }
+//
+// export function handleAddPool(pool) {
+//   return (dispatch, getState) => {
+//     const { authedUser } = getState()
+//
+//     dispatch(showLoading)
+//
+//   }
+// }
