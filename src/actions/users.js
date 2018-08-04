4881f69274f4f3c6580ffb217633089fc163ebda
Nicholas <nicholas.allio@me.com>
2018-08-04 21:03:16 +0200

    Implemente action to retrieve users and added to the store

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,23 @@
+import { _getUsers } from '../utils/_DATA'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const GET_USERS = 'GET_USERS'
 
-export function getUsers(users) {
+function getUsers(users) {
   return {
     type: GET_USERS,
     users
+  }
+}
+
+export function handleGetUsers() {
+  return (dispatch) => {
+    dispatch(showLoading())
+    return _getUsers()
+      .then((users) => {
+        dispatch(getUsers(users))
+        dispatch(hideLoading())
+      })
+      .catch(() => alert('There was an error retrieving users. Try again.'))
   }
 }
