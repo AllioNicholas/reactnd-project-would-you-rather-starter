11d35b09e30b0125ad97344d1fef3a188d41ab97
Nicholas <nicholas.allio@me.com>
2018-08-04 19:43:29 +0200

    Created identification page component with initial dummy implementation

---
+++ src/components/IdentificationPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class IdentificationPage extends Component {
+  render() {
+    return {
+      <div>
+        Identification page
+      </div>
+    }
+  }
+}
+
+export default IdentificationPage
