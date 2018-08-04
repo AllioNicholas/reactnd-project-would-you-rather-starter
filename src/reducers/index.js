83fc71344584a319d529d21ea4b01b1853775512
Nicholas <nicholas.allio@me.com>
2018-08-04 19:48:29 +0200

    Created index and authedUser reducres. authedUser implemented. index exports combine reducers atm with authedUser and loadingBar

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading'
+
+import authedUser from './authedUser'
+
+export default combineReducers({
+  authedUser,
+  loadingBar: loadingBarReducer
+})
