f4c7a2e27488efcd40bf98a5ffabfe9474b9fe14
Nicholas <nicholas@ulmon.com>
2018-08-07 17:00:58 +0200

    Converted in regular component and printed some info about it

--- src/components/Pool.js
+++ src/components/Pool.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const Pool = (props) => {
-  return(
-    <div>
-      {props.id}
-    </div>
-  )
+class Pool extends Component {
+  render() {
+    const { pool } = this.props
+    const { id, author, timestamp, optionOne, optionTwo } = pool
+
+    return(
+      <Link to={`/questions/${id}`}>
+        <p>{id}</p>
+        <p>{author}</p>
+        <p>{timestamp}</p>
+      </Link>
+    )
+  }
 }
 
-export default Pool
+function mapStateToProps({ pools }, { id }) {
+  const pool = pools[id]
+
+  return {
+    pool
+  }
+}
+
+export default connect(mapStateToProps)(Pool)
