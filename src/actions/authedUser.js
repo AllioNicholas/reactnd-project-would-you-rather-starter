38e61ff6f9c6f47e93ed1f5ecf11c901a4ca7ad5
Nicholas <nicholas@ulmon.com>
2018-08-07 11:04:36 +0200

    Implemented handle set authed user to dispatch action creator

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,8 +1,18 @@
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const SET_AUTHEDUSER = 'SET_AUTHEDUSER'
 
-export function setAuthedUser (id) {
+function setAuthedUser (id) {
   return {
     type: SET_AUTHEDUSER,
     id
+  }
+}
+
+export function handleSetAuthedUser(id) {
+  return (dispatch) => {
+    dispatch(showLoading())
+    dispatch(setAuthedUser(id))
+    dispatch(hideLoading())
   }
 }
