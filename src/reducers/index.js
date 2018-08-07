cd84f3a165958b79e0d2ac0cafe124ba9cf32e9f
Nicholas <nicholas@ulmon.com>
2018-08-07 11:16:38 +0200

    Combined pools reducer with others in index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { loadingBarReducer } from 'react-redux-loading'
 
 import authedUser from './authedUser'
 import users from './users'
+import pools from './pools'
 
 export default combineReducers({
   authedUser,
   users,
+  pools,
   loadingBar: loadingBarReducer
 })
