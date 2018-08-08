40ac5b581c8b56a4e1f30ec6784d4b43ed5d81bf
Nicholas <nicholas@ulmon.com>
2018-08-08 15:39:55 +0200

    Added voting logic and UI update to display or not voting possibility

--- src/components/PoolPage.js
+++ src/components/PoolPage.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
 import Pool from './Pool'
+import { handleAddAnswer } from '../actions/pools'
 
 class PoolPage extends Component {
-  render() {
+
+  handleAnswer = (e) => {
+    e.preventDefault()
+
     const { id } = this.props
+    const option = e.target.value
+
+    this.props.dispatch(handleAddAnswer(id, option))
+  }
+
+  render() {
+    const { id, thisPool, authedUser } = this.props
+    const poolAnswered = thisPool.optionOne.votes.includes(authedUser) || thisPool.optionTwo.votes.includes(authedUser)
+
+    if (thisPool === null) {
+      return <Redirect to='/notfound' />
+    }
+
     return (
       <div>
         PoolPage
         <Pool id={id} />
+        {poolAnswered === false && <div>
+          <button
+            value='optionOne'
+            onClick={this.handleAnswer}
+            >
+            Option one
+          </button>
+          <button
+            value='optionTwo'
+            onClick={this.handleAnswer}
+            >
+            Option two
+          </button>
+        </div>}
       </div>
     )
   }
 }
 
-function mapStateToProps({ authedUser, pools }, props) {
+function mapStateToProps({ authedUser, pools, users }, props) {
   const { id } = props.match.params
+  const thisPool = pools[id]
 
   return {
-    id
+    id,
+    authedUser,
+    thisPool: thisPool ? thisPool : null
   }
 }
 
