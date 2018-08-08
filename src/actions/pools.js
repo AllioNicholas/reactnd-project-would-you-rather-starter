ae011a69c1c6cbe84b06054a08834a72de46ae95
Nicholas <nicholas@ulmon.com>
2018-08-08 15:37:48 +0200

    Added action creator and handle action for saving pool answer

--- src/actions/pools.js
+++ src/actions/pools.js
@@ -5,6 +5,34 @@ export const GET_POOLS = 'GET_POOLS'
 export const ADD_POOL = 'ADD_POOL'
 export const ADD_ANSWER = 'ADD_ANSWER'
 
+function addAnswer(pool) {
+  return {
+    type: ADD_ANSWER,
+    pool
+  }
+}
+
+export function handleAddAnswer(id, option) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    dispatch(showLoading())
+
+    const poolAnswer = {
+      authedUser,
+      qid: id,
+      answer: option
+    }
+
+    return API._saveQuestionAnswer(poolAnswer)
+      .then(() => {
+        dispatch(addAnswer(poolAnswer))
+        dispatch(hideLoading())
+      })
+      .catch(() => alert('There was an error saving your answer. Try again.'))
+  }
+}
+
 function getPools(pools) {
   return {
     type: GET_POOLS,
