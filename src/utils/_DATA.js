435f43f5360e517789acbcea9fcbe61d82875c8a
Nicholas <nicholas@ulmon.com>
2018-08-08 15:50:18 +0200

    Deleted static images. Added URLs to cool avatar service

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: './images/2.png',
+    avatarURL: 'https://ui-avatars.com/api/?name=Sarah+Edo',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionOne',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: './images/1.png',
+    avatarURL: 'https://ui-avatars.com/api/?name=Tyler+McGinnis',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: './images/3.png',
+    avatarURL: 'https://ui-avatars.com/api/?name=John+Doe',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
