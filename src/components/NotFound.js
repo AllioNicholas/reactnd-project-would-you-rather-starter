5358c64c7abd32ea9e6f2d2db301d750fb8805a5
Nicholas <nicholas@ulmon.com>
2018-08-08 12:10:52 +0200

    Created first functional component to display 404 page

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404</h2>
+      <h3>This is not the page you are looking for</h3>
+    </div>
+  )
+}
